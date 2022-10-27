86fae3f90cf653d32d572542fe6e5bafa995c3bc
sugyoung lee <tnrud4685@naver.com>
2022-10-27 17:27:06 +0900

    interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import styled, { keyframes } from "styled-components";
-
-// styled-components practice
-
-/* const Input = styled.input.attrs({ required: true })`
-  // → 컴포넌트 속성값 설정 가능!
-  background-color: tomato;
-`; */
-// as="" 을 사용하여 엘리먼트를 다른 엘리먼트로 교체할 수 있다!
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vh;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
