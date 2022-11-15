b21c3ab5b274dcece15df76da21b180228625f5b
sugyoung lee <tnrud4685@naver.com>
2022-11-16 00:47:32 +0900

    coin tracking practice2

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,6 +8,9 @@ function Router() {
       <Routes>
         <Route path="/" element={<Coins />}></Route>
         <Route path="/:coinId" element={<Coin />}></Route>
+        <Route path="/:coinId/*" element={<Coin />} />
+        {/* 마지막에 /*를 적어 명시적으로 이 route의 내부에서 nested route가 render 될 수 있음을 표시하고,
+        자식 route를 부모 route의 element 내부에 작성하는 방법 사용 */}
       </Routes>
     </BrowserRouter>
   );
